3d67aeecdb3927e140ccc951472dbea9f54f38dc
amithsoddy <anithasoddy@gmail.com>
2024-10-25 20:46:56 +0530

    list to dictionary

---
+++ list to dictionary.py
@@ -0,0 +1,12 @@
+def test(st):
+    result={}
+    for item in list:
+        result[item[0]] =item[1:]
+        return result
+    cars= [["defender b 200"]], [["merceedes g wagon"]], [["range rover velar"]], [["thar"]], [["bmw x-7 series"]]
+    print("\n original list of lists:")
+    print(cars)
+    print("\nconverted lists to a dictionary:")
+    print(test(cars))
+
+    
